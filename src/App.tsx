47829f0fee3fd5beafc25835cbdbc62a85246706
amit638824 <amit.chauhan@techwagger.com>
2026-02-17 08:33:35 +0530

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import LoginPage from "./pages/common/login/SignIn";
 import Layout from "./components/layout/Layout";
- 
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} /> //url changes............
         <Route path="/dashboard" element={<Layout>  <Dashboard />  </Layout>} />
       </Routes>
     </BrowserRouter>
